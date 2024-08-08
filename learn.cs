c44e18da7ab5285145de39fe733e6d1383998485
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-08-08 15:49:01 +0300

    Consol.Writeline

---
+++ learn.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Giraffe
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");
+            Console.ReadLine();
+        }
+    }
+}
