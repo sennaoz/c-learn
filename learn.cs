e4f379edc28c35da656c5142c66b841233ffa07c
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-08-08 19:37:12 +0300

    IndexOf

--- learn.cs
+++ learn.cs
@@ -11,7 +11,7 @@ namespace Giraffe
         static void Main(string[] args)
         {
             string phrase = "Giraffe Academy";
-            Console.WriteLine(phrase[0]); // 1. karakter ne 0 1 2 3 diye karakter gider output G
+            Console.WriteLine(phrase.IndexOf("A")); // kacıncı indexte başlıyor eger olmayan bir sey ise -1
 
             Console.ReadLine();
         }
