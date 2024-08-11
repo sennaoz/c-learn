209467dd4ffddd9ba2d0eb147f6465292ddc6549
sennaoz <161388670+sennaoz@users.noreply.github.com>
2024-08-12 00:22:12 +0300

    calculator with if

--- learn.cs
+++ learn.cs
@@ -6,21 +6,33 @@ using System.Threading.Tasks;
 namespace Giraffe
 
 {
-    
+
     class Program
     {
 
             static void Main(string[] args)
         {
      
-            Console.Write("enter your name: ");
-            string name = Console.ReadLine();
+            Console.Write("enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter a number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("choice process:(+ , -  , / , * ,  :)");
+            string process = Console.ReadLine();
+
+            if (process == "+")
+                Console.WriteLine(num1 + "+" + num2 + "=" + num1 + num2);
+            if (process == "-")
+                Console.WriteLine(num1 + "-" + num2 + "=" + (num1 - num2));
+            if (process == "/")
+                Console.WriteLine(num1 + "/" + num2 + "=" + num1 / num2);
+
+            if (process == "*")
+                Console.WriteLine(num1 + "*" + num2 + "=" + num1 * num2);
 
 
-            Console.WriteLine("Hello " + name);
 
-           
-           
             Console.ReadLine();
         }
 
